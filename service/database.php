0407fe1f531839739e07ac66e35f4fbf7c63df84
Ryan <64879948+YannYam@users.noreply.github.com>
2025-11-08 12:14:10 +0700

    Replace mysqli with PDO and add data retrieval functions
    Refactor database connection to use PDO and add several functions for data retrieval.

--- service/database.php
+++ service/database.php
@@ -1,13 +1,56 @@
-<?php 
-	$hostname = "localhost";
-	$username = "root";
-	$password = "";
-	$database_name = "perpustakaan_anak";
-
-	$db = mysqli_connect($hostname, $username, $password, $database_name);
-
-	if($db -> connect_error) {
-		echo "koneksi gagal";
-		die("error!");
+<?php
+	try {
+		$db = new PDO("mysql:host=localhost;dbname=perpustakaan;charset=utf8mb4", "root", "");
+	
+	} catch (PDOException $e) {
+		echo "Koneksi Gagal";
 	}
-?>
+
+	function required($data) {
+		return $data == "";
+	}
+
+	function alfabet($data) {
+		return preg_match("/^[a-zA-Z\s]+$/", $data);
+	}
+
+	function numerik($data){
+		return preg_match("/^[0-9]+$/", $data);
+	}
+
+	function getBuku(){
+		global $db;
+		$state = $db->prepare('SELECT * FROM buku');
+		$state->execute();
+		$artikel = $state->fetchAll();
+		return $artikel;
+	}
+
+	function getBukuOne($data){
+		global $db;
+		$state = $db->prepare("SELECT * FROM buku WHERE ID_BUKU = '$data'");
+		$state->execute();
+		$artikel = $state->fetch();
+		return $artikel;
+	}
+
+	function getPemustaka(){
+		global $db;
+		$state = $db->prepare("SELECT * FROM pemustaka WHERE PERAN = 'pemustaka'");
+		$state->execute();
+		$artikel = $state->fetchAll();
+		return $artikel;
+	}
+
+	function getDaftarPeminjaman() {
+	    global $db;
+	    $sql = "SELECT buku.JUDUL_BUKU, buku.PENULIS, buku.PENERBIT, buku.TAHUN,
+	                   peminjaman.STATUS, pemustaka.NAMA_USER, peminjaman.TANGGAL_PINJAM
+	            FROM peminjaman
+	            JOIN buku ON peminjaman.ID_BUKU = buku.ID_BUKU
+	            JOIN pemustaka ON peminjaman.ID_USER = pemustaka.ID_USER";
+	    
+	    $stmt = $db->prepare($sql);
+	    $stmt->execute();
+	    return $stmt->fetchAll();
+	}
