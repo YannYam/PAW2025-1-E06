0106f85e1139ecce41d61dd471d63295e3d60b7b
denisatrianaputri <denisatrianap@gmail.com>
2025-11-13 13:44:35 +0700

    function tambah validasi

--- function.php
+++ function.php
@@ -5,13 +5,51 @@ require_once(BASE_PATH . '/service/connect.php');
 		return $data == "";
 	}
 
+	function test_input($data){
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+
+	function wajib($data) {
+		return !empty($data);
+	}
+
 	function alfabet($data) {
 		return preg_match("/^[a-zA-Z\s]+$/", $data);
 	}
 
-	function numerik($data){
-		return preg_match("/^[0-9]+$/", $data);
+	function numerik($data) {
+		return is_numeric($data);
 	}
+
+	function email($data) {
+		return filter_var($data, FILTER_VALIDATE_EMAIL);
+	}
+
+	function alfanumerik($data) {
+		return preg_match("/^[A-Za-z0-9]+$/", $data);
+	}
+
+	function password($data) {
+		return preg_match("/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).+$/", $data);
+	}
+
+	function username($data) {
+		return preg_match("/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z0-9]+$/", $data);
+	}
+
+	function alamat($data) {
+		return preg_match("/^(?=.*[A-Za-z])[A-Za-z0-9\s.,\/#()'-]+$/", $data);
+	}
+
+	function valid_tanggal($data) {
+		$d = DateTime::createFromFormat("Y-m-d", $data);
+		return $d && $d->format("Y-m-d") === $data;
+	}
+
+
 
 	function tambahBuku(array $data){
 		$state = DBH->prepare("INSERT INTO buku (JUDUL_BUKU, DESKRIPSI, PENULIS, PENERBIT, TAHUN, STOK) VALUES (:judul, :deskripsi, :penulis, :penerbit, :tahun, :stok)");
