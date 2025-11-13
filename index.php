2d91b02d4f738e4003e0b98d5a76fbae6529be35
denisatrianaputri <denisatrianap@gmail.com>
2025-11-13 17:56:19 +0700

    tampilan

--- index.php
+++ index.php
@@ -1,7 +1,78 @@
-<h1>Halaman Utama</h1>
+<?php  
+require_once 'function.php'; 
 
-<h1> tes update </h1>
+$username = $password = '';
 
-<h1> menambahkan ini mengubah ini </h1>
+$error_username = $error_password = '';
 
-<p> tes tes tes tes  </p>
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $username = test_input($_POST['username']);
+    $password = test_input($_POST['password']);
+
+    // Validasi Username
+    if (!wajib($username)) {
+        $error_username = 'Username wajib diisi.';
+    } elseif (!username($username)) {
+        $error_username = 'Username hanya boleh berisi huruf dan angka.';
+    } elseif (strlen($username) < 5 || strlen($username) > 20) {
+        $error_username = 'Username minimal 5 karakter dan maksimal 20 karakter.';
+    }
+
+    // Validasi Password
+    if (!wajib($password)) {
+        $error_password = 'Password wajib diisi.';
+    } elseif (!password($password)) {
+        $error_password = 'Password harus mengandung huruf dan angka.';
+    } elseif (strlen($password) < 5) {
+        $error_password = 'Password minimal 5 karakter.';
+    }
+
+    if (empty($error_username) && empty($error_password)) {
+        echo "Sukses Login!";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Libra</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+    <!-- navbar -->
+    <header class="navbar">
+        <img src="libra.jpg" class="logo" alt="logo">
+        <h2>Libra</h2>
+
+    </header>
+
+    <!-- Card Login -->
+    <div class="container">
+        <div class="card-login">
+            <h2>Login Akun</h2>
+            <p>“Selamat Datang Kembali.”</p>
+
+            <form action="" method="POST">
+
+                
+                <input type="text" placeholder="Username"name="username" value="<?= $username ?>">
+                <div class="error"><?= $error_username ?></div>
+
+                <input type="password" placeholder="Password"name="password" value="<?= $password ?>">
+                <div class="error"><?= $error_password ?></div>
+
+                <button type="submit" class="login-btn">Login</button>
+
+            </form>
+
+            <a href="register.php">Daftar Akun</a>
+        </div>
+    </div>
+
+</body>
+</html>
